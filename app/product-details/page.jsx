a8c4e26c77ce716ff7e128d1cd72bdaeece9af73
faysal hossain <devfaysalbd@gmail.com>
2024-10-25 11:46:15 +0600

    some update

--- app/product-details/page.jsx
+++ app/product-details/page.jsx
@@ -20,65 +20,65 @@ const productDetailsPage = () => {
     return (
         <>
             <Breadcrumb />
-            <div class="container mx-auto grid grid-cols-2 gap-6">
+            <div className="container mx-auto grid grid-cols-2 gap-6">
                 <div>
-                    <Image src={Product1} alt="product" class="w-full" />
-                    <div class="grid grid-cols-5 gap-4 mt-4">
+                    <Image src={Product1} alt="product" className="w-full" />
+                    <div className="grid grid-cols-5 gap-4 mt-4">
                         <Image src={Product2} alt="product2"
-                            class="w-full cursor-pointer border border-primary" />
-                        <Image src={Product3} alt="product2" class="w-full cursor-pointer border" />
-                        <Image src={Product4} alt="product2" class="w-full cursor-pointer border" />
-                        <Image src={Product5} alt="product2" class="w-full cursor-pointer border" />
-                        <Image src={Product6} alt="product2" class="w-full cursor-pointer border" />
+                            className="w-full cursor-pointer border border-primary" />
+                        <Image src={Product3} alt="product2" className="w-full cursor-pointer border" />
+                        <Image src={Product4} alt="product2" className="w-full cursor-pointer border" />
+                        <Image src={Product5} alt="product2" className="w-full cursor-pointer border" />
+                        <Image src={Product6} alt="product2" className="w-full cursor-pointer border" />
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-3xl font-semibold uppercase mb-2">Italian L Shape Sofa</h2>
-                    <div class="flex items-center mb-4">
-                        <div class="flex gap-1 text-sm text-yellow-400">
+                    <h2 className="text-3xl font-semibold uppercase mb-2">Italian L Shape Sofa</h2>
+                    <div className="flex items-center mb-4">
+                        <div className="flex gap-1 text-sm text-yellow-400">
                             <span><FaStar /></span>
                             <span><FaStar /></span>
                             <span><FaStar /></span>
                             <span><FaStar /></span>
                             <span><FaStar /></span>
                         </div>
-                        <div class="text-xs text-gray-500 ml-3">(150 Reviews)</div>
+                        <div className="text-xs text-gray-500 ml-3">(150 Reviews)</div>
                     </div>
-                    <div class="space-y-2">
-                        <p class="text-gray-800 font-semibold space-x-2">
+                    <div className="space-y-2">
+                        <p className="text-gray-800 font-semibold space-x-2">
                             <span>Availability: </span>
-                            <span class="text-green-600">In Stock</span>
+                            <span className="text-green-600">In Stock</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">Brand: </span>
-                            <span class="text-gray-600">Apex</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">Brand: </span>
+                            <span className="text-gray-600">Apex</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">Category: </span>
-                            <span class="text-gray-600">Sofa</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">Category: </span>
+                            <span className="text-gray-600">Sofa</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">SKU: </span>
-                            <span class="text-gray-600">BE45VGRT</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">SKU: </span>
+                            <span className="text-gray-600">BE45VGRT</span>
                         </p>
                     </div>
-                    <div class="flex items-baseline mb-1 space-x-2 font-roboto mt-4">
-                        <p class="text-xl text-primary font-semibold">$45.00</p>
-                        <p class="text-base text-gray-400 line-through">$55.00</p>
+                    <div className="flex items-baseline mb-1 space-x-2 font-roboto mt-4">
+                        <p className="text-xl text-primary font-semibold">$45.00</p>
+                        <p className="text-base text-gray-400 line-through">$55.00</p>
                     </div>
 
-                    <p class="mt-4 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eius eum
+                    <p className="mt-4 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eius eum
                         reprehenderit dolore vel mollitia optio consequatur hic asperiores inventore suscipit, velit
                         consequuntur, voluptate doloremque iure necessitatibus adipisci magnam porro.</p>
 
 
-                    <div class="mt-4">
-                        <h3 class="text-sm text-gray-800 uppercase mb-1">Quantity</h3>
-                        <div class="flex border border-gray-300 text-gray-600 divide-x divide-gray-300 w-max">
-                            <div class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">-</div>
-                            <div class="h-8 w-8 text-base flex items-center justify-center">4</div>
-                            <div class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">+</div>
+                    <div className="mt-4">
+                        <h3 className="text-sm text-gray-800 uppercase mb-1">Quantity</h3>
+                        <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300 w-max">
+                            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">-</div>
+                            <div className="h-8 w-8 text-base flex items-center justify-center">4</div>
+                            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">+</div>
                         </div>
                     </div>
 
@@ -93,17 +93,17 @@ const productDetailsPage = () => {
                             className="w-[200px] py-[10px] text-center text-white bg-primary hover:bg-black transition-all duration-300 flex items-center justify-center gap-3 font-semibold rounded "><FaCartShopping /> Add to cart</Link>
                     </div>
 
-                    <div class="flex gap-3 mt-4">
+                    <div className="flex gap-3 mt-4">
                         <Link href="#"
-                            class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                            className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <FaFacebookF />
                         </Link>
                         <Link href="#"
-                            class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                            className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <FaTwitter />
                         </Link>
                         <Link href="#"
-                            class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                            className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <FaInstagram />
                         </Link>
                     </div>
@@ -111,10 +111,10 @@ const productDetailsPage = () => {
             </div>
 
             {/* start the product description section */}
-            <div class="container mx-auto pb-16 mt-10">
-                <h3 class="border-b border-gray-200 font-roboto text-gray-800 pb-3 font-semibold text-2xl">Product details</h3>
-                <div class="w-3/5 pt-6">
-                    <div class="text-gray-600">
+            <div className="container mx-auto pb-16 mt-10">
+                <h3 className="border-b border-gray-200 font-roboto text-gray-800 pb-3 font-semibold text-2xl">Product details</h3>
+                <div className="w-3/5 pt-6">
+                    <div className="text-gray-600">
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus deleniti natus
                             dolore cum maiores suscipit optio itaque voluptatibus veritatis tempora iste facilis non aut
                             sapiente dolor quisquam, ex ab.</p>
